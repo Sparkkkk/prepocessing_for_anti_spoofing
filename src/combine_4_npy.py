4107e34298c94d40f964ff94fa0353871217b5fe
Sparkkkk <sparksun37@gmail.com>
2018-06-25 13:29:59 -0700

    added function to split data into training set, validation set, and testing set

--- src/combine_4_npy.py
+++ src/combine_4_npy.py
@@ -8,6 +8,7 @@ data_path = os.path.join(sys.path[0], '..', 'data')
 temp_data_path = os.path.join(data_path, 'temp_data')
 dataset_path = os.path.join(data_path, 'dataset')
 clean_data_path = os.path.join(data_path, 'clean_data')
+new_dataset_path = os.path.join(data_path, 'anti_spoofing_video_dataset', 'npy')
 
 def save_fake_face_npz():
     v0 = tl.files.load_npy_to_any(temp_data_path, 'video0.npy')
@@ -25,47 +26,62 @@ def save_fake_face_npz():
     tl.files.save_any_to_npy(dictionary, os.path.join(dataset_path, 'fake.npy'))
 
 
-print('loading data')
-print(dataset_path)
-real = tl.files.load_npy_to_any(clean_data_path, 'real.npy')
-fake = tl.files.load_npy_to_any(clean_data_path, 'fake.npy')
-mine = tl.files.load_npy_to_any(dataset_path, 'employee.npy')
-video4 = tl.files.load_npy_to_any(temp_data_path, 'video4.npy')
-video5 = tl.files.load_npy_to_any(temp_data_path, 'video5.npy')
-video6 = tl.files.load_npy_to_any(temp_data_path, 'video6.npy')
-video7 = tl.files.load_npy_to_any(temp_data_path, 'video7.npy')
-
-X = np.append(real['real_faces'], fake['fake_faces'], 0)
-y = np.append(real['labels'], fake['labels'], 0)
-X = np.append(X, mine['X'], axis=0)
-y = np.append(y, mine['y'], axis=0)
-X = np.append(X, video4['X'], axis=0)
-X = np.append(X, video5['X'], axis=0)
-X = np.append(X, video6['X'], axis=0)
-X = np.append(X, video7['X'], axis=0)
-y = np.append(y, video4['y'], axis=0)
-y = np.append(y, video5['y'], axis=0)
-y = np.append(y, video6['y'], axis=0)
-y = np.append(y, video7['y'], axis=0)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3)
-X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=.3)
-
-dataset = {
-    'X_train': X_train,
-    'y_train': y_train,
-    'X_valid': X_valid,
-    'y_valid': y_valid,
-    'X_test': X_test,
-    'y_test': y_test
-}
-
-print('saving dataset')
-
-tl.files.save_any_to_npy(dataset, os.path.join(dataset_path, 'dataset_new.npy'))
+def save_dataset():
+    print('loading data')
+    print(dataset_path)
+    real = tl.files.load_npy_to_any(clean_data_path, 'real.npy')
+    fake = tl.files.load_npy_to_any(clean_data_path, 'fake.npy')
+    mine = tl.files.load_npy_to_any(dataset_path, 'employee.npy')
+    video4 = tl.files.load_npy_to_any(temp_data_path, 'video4.npy')
+    video5 = tl.files.load_npy_to_any(temp_data_path, 'video5.npy')
+    video6 = tl.files.load_npy_to_any(temp_data_path, 'video6.npy')
+    video7 = tl.files.load_npy_to_any(temp_data_path, 'video7.npy')
+    X = np.append(real['real_faces'], fake['fake_faces'], 0)
+    y = np.append(real['labels'], fake['labels'], 0)
+    X = np.append(X, mine['X'], axis=0)
+    y = np.append(y, mine['y'], axis=0)
+    X = np.append(X, video4['X'], axis=0)
+    X = np.append(X, video5['X'], axis=0)
+    X = np.append(X, video6['X'], axis=0)
+    X = np.append(X, video7['X'], axis=0)
+    y = np.append(y, video4['y'], axis=0)
+    y = np.append(y, video5['y'], axis=0)
+    y = np.append(y, video6['y'], axis=0)
+    y = np.append(y, video7['y'], axis=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3)
+    X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=.3)
+    dataset = {
+        'X_train': X_train,
+        'y_train': y_train,
+        'X_valid': X_valid,
+        'y_valid': y_valid,
+        'X_test': X_test,
+        'y_test': y_test
+    }
+    print('saving dataset')
+    tl.files.save_any_to_npy(dataset, os.path.join(dataset_path, 'dataset_new.npy'))
 
 
+def load_dataset():
+    X = np.ndarray([0, 8, 128, 128, 3])
+    X = np.append(X, tl.files.load_npy_to_any(new_dataset_path, 'G_training_0.npy')['X'])
+    X = np.append(X, tl.files.load_npy_to_any(new_dataset_path, 'G_training_1.npy')['X'])
+    count = X.shape[0]
+    y = np.zeros(count)
+    X = np.append(X, tl.files.load_npy_to_any(new_dataset_path, 'F_training_0.npy')['X'])
+    X = np.append(X, tl.files.load_npy_to_any(new_dataset_path, 'F_training_1.npy')['X'])
+    X = np.append(X, tl.files.load_npy_to_any(new_dataset_path, 'F_training_2.npy')['X'])
+    X = np.append(X, tl.files.load_npy_to_any(new_dataset_path, 'F_training_3.npy')['X'])
+    X = np.append(X, tl.files.load_npy_to_any(new_dataset_path, 'F_training_4.npy')['X'])
+    X = np.append(X, tl.files.load_npy_to_any(new_dataset_path, 'F_training_5.npy')['X'])
+    count_diff = X.shape[0] - count
+    y = np.append(y, np.ones(count_diff))
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3)
+    X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=.3)
+    return X_train, y_train, X_valid, y_valid, X_test, y_test
 
 
+if __name__ == '__main__':
+    print(sys.path[0])
 
 
--- src/new_dataset_clean/extract_face.py
+++ src/new_dataset_clean/extract_face.py
@@ -12,7 +12,8 @@ testing = os.path.join(dataset, 'testing')
 
 def extract_face(training: bool, real: bool, number_frame_per_object=8, jump_frames=7) -> np.ndarray:
     path = os.path.join(dataset, 'training') if training else os.path.join(dataset, 'testing')
-    X = np.ndarray([0, number_frame_per_object, 128, 128, 3])
+    # X = np.ndarray([0, number_frame_per_object, 128, 128, 3])
+    X = []
     folders_name = list(os.walk(path))[1:]
     for folder in folders_name:
         print(folder)
@@ -24,18 +25,20 @@ def extract_face(training: bool, real: bool, number_frame_per_object=8, jump_fra
                 if prefix_condition:
                     x = extract_faces_for_3d_cnn(number_frame_per_object, video_name, jump_frames, video_path)
                     if x.shape == (x.shape[0], number_frame_per_object, 128, 128, 3):
-                        X = np.append(X, x, axis=0)
+                        # X = np.append(X, x, axis=0)
+                        X.append(x)
                         print('finished %s.mp4' % video_name)
                     else:
                         print('failed to append %s.mp4' % video_name)
+    X = np.asarray(X)
     print(X.shape)
     return X
 
 
 def main():
-    content = extract_face(True, False)
+    content = extract_face(training=False, real=False)
     path = os.path.join(dataset, 'npy')
-    save_large_npy_file(content, path, 'F_training', False, 500)
+    save_large_npy_file(content, path, 'F_testing', real=False, size_for_each_file=500)
 
 
 if __name__ == '__main__':
